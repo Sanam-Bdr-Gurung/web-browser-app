fd504efb804e023e0625702e0492858012aa6f8d
Sanam Gurung <sanam.bdr.gurung@gmail.com>
2024-08-15 21:54:45 +0545

    Allow users to choose a website using UIAlertController and Action sheets

--- web-browser-app/ViewController.swift
+++ web-browser-app/ViewController.swift
@@ -19,11 +19,29 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.youtube.com/@nepalharmony5589")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
-
+   
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "youtube.com/@sukilamavlogs", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "youtube.com/@nepalharmony5589", style: .default, handler: openPage))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
