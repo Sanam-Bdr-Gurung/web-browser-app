2151be097aea1c1bab737a43a16a8b5e2a496fad
Sanam Gurung <sanam.bdr.gurung@gmail.com>
2024-08-16 11:41:48 +0545

    add refrehs functionality and show the reloaded progress

--- web-browser-app/ViewController.swift
+++ web-browser-app/ViewController.swift
@@ -10,7 +10,8 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
-    
+    var progressView: UIProgressView!
+    var websites = ["youtube.com/@nepalharmony5589","youtube.com/@sukilamavlogs"]
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -20,16 +21,28 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
     
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
         
-        let url = URL(string: "https://www.youtube.com/@nepalharmony5589")!
+
+        toolbarItems = [progressButton,spacer,refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress),options: .new,context: nil)
+        
+        let url = URL(string: websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
    
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        
+        
         ac.addAction(UIAlertAction(title: "youtube.com/@sukilamavlogs", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "youtube.com/@nepalharmony5589", style: .default, handler: openPage))
         
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
@@ -42,6 +55,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 }
 
